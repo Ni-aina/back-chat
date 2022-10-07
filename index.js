473c670ed9d0237cb7a0f951e848ffb888827c01
Nï Aina <niandriah.l@gmail.com>
2022-10-07 11:25:09 +0300

    ProcFile

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 const db = mysql.createConnection({
     host: 'mysql-aina.alwaysdata.net',
     user: 'aina',
